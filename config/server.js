e21f95f70665afd7f11724ddedcba16d251f72d4
Lucas Rombini <83418676+luckpapurro@users.noreply.github.com>
2022-08-31 02:44:10 -0300

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9ef92c50b6af10eb3d60b67b9ee3489'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e9986326-9a41-4155-b3bc-85971d6671ef'
+};
